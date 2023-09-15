f6d68eb174c0afbcd1f5cebc72a69e59f7471f2b
Angelo Bonini <abonini2000@gmail.com>
2023-09-15 17:31:36 -0300

    (feature): implements a query

--- src/index.js
+++ src/index.js
@@ -2,14 +2,32 @@ const { ApolloServer } = require ('apollo-server');
 
 
 const typeDefs =`
-type Query{
+type Query {
     info: String!
+    feed: [Link!]!
+}
+type Link {
+    id: ID!
+    description: String!
+    url: String!
 }
 `
+let links = [{
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL'
+  }]
+
 const resolvers = {
     Query:{
-        info:()=> "{ I'm alive and serving content}"
-    }
+        info:()=> "my hackernews clone",
+        feed: () => links,
+    },
+    Link: {
+        id: (parent) => parent.id,
+        description: (parent) => parent.description,
+        url: (parent) => parent.url,
+      }
 }
 
 const server = new ApolloServer({typeDefs, resolvers})
