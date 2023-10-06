0a5930438850cc064244f397c1736ecbd0594c14
Angelo Bonini <abonini2000@gmail.com>
2023-10-06 17:17:25 -0300

    (algorithms): function to chekc if a integer is even

---
+++ src/algorithms/isEven.js
@@ -0,0 +1,13 @@
+function isEven(possibleEven){
+    while(possibleEven > 2){
+        possibleEven = possibleEven - 2
+        console.log(`now the number is: ` + possibleEven)
+    }
+    const oddOrEven = possibleEven == 2
+    console.log(oddOrEven``)
+    return oddOrEven
+
+}
+
+isEven(4);
+isEven(5)
